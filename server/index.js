ba98047e8c5af248a44ecd03f5974474ca456fa8
bpn1604 <bipin.kumar2015@vitalum.ac.in>
2024-03-20 10:21:06 +0530

    Routes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,14 @@ const express = require('express')
 const app = express()
 
 const db = require("./models")
+
+
+//Routers 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+const postRouter = require("./routes/Posts")
+
+app.use("/posts" , postRouter)
 db.sequelize.sync().then(() =>{
     app.listen(8080,() => {
         console.log('server running on 8080')
